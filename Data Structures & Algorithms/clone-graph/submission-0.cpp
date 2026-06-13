654809177816f5185e13b66f8bac0b24a65d156d
Prajjwal Shukla <prajjwal.shukla44@gmail.com>
2026-06-13 16:05:28 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/clone-graph/submission-0.cpp
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    unordered_map<Node*, Node*> vis;
+    
+    Node* cloneGraph(Node* node) {
+        if(vis[node]) return vis[node];
+        if(!node) return NULL;
+        Node* newNode = new Node(node->val);
+        vis[node] = newNode;
+
+        for(Node* x: node->neighbors) newNode->neighbors.push_back(cloneGraph(x));
+        return newNode;    
+    }
+};
---
+++ Data Structures & Algorithms/valid-tree/submission-1.cpp
@@ -0,0 +1,41 @@
+class Solution {
+    //check edges == n-1 and all nodes connected
+public:
+     void BFS(vector<vector<int>>&adj, vector<bool>&vis, int s){
+        
+        queue<int>q;
+        q.push(s);
+        vis[s] = 1;
+
+        while(!q.empty()){
+            int curr = q.front();
+            q.pop();
+            for(int i: adj[curr]){
+                if(!vis[i]){
+                    vis[i] = 1;
+                    q.push(i);
+                }
+            }
+        }
+    }
+
+    bool connected(vector<vector<int>>& edges, int n){
+        vector<vector<int>>adj(n);
+        for(int i=0;i<edges.size();i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+        }
+
+        int ans = 0;
+        vector<bool>vis(n);
+        for(int i=0;i<adj.size();i++) {
+            if(!vis[i]) BFS(adj, vis, i), ans++;
+            if(ans > 1) return 0;
+        }
+        return 1;
+    }
+    
+    bool validTree(int n, vector<vector<int>>& edges) {
+        return connected(edges, n) && (edges.size() == n-1);
+    }
+};
