f0cfec467e857999d1817d2a3b51bdeda85cf6d6
Prajjwal Shukla <prajjwal.shukla44@gmail.com>
2026-06-24 22:35:32 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0;
+        for(int j=0;j<nums.size();j++){
+            if(nums[j]!=val) swap(nums[i++], nums[j]);
+        }
+        return i;
+    }
+};
