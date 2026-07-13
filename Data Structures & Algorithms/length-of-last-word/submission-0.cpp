cff121171051f6c482b9595779d81cd644985b6a
Prajjwal Shukla <prajjwal.shukla44@gmail.com>
2026-07-13 22:54:12 +0530

    Add: length-of-last-word - submission-0

---
+++ Data Structures & Algorithms/length-of-last-word/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int ans = 0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s[i] == ' ')  if (ans == 0) continue; else break;
+            else ans++;
+        }
+        return ans;
+    }
+};
