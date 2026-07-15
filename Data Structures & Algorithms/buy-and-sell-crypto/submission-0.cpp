81f18c2a7b3ef76a9725542229cb5502d7a9a48f
Prajjwal Shukla <prajjwal.shukla44@gmail.com>
2026-07-15 14:21:18 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini = prices[0], profit = 0;
+        for(int i: prices){
+            int cost = i - mini;
+            profit = max(cost, profit);
+            mini = min(i, mini);
+        }
+        return profit;
+    }
+};
