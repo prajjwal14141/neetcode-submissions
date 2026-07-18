daea32a6c2765304ed38546f86d1c78aca99d6e8
Prajjwal Shukla <prajjwal.shukla44@gmail.com>
2026-07-19 00:17:58 +0530

    Add: maximum-ascending-subarray-sum - submission-3

---
+++ Data Structures & Algorithms/maximum-ascending-subarray-sum/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int sum = nums[0], ans = sum;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i] > nums[i-1]) {
+                sum += nums[i];
+                ans = max(sum, ans);
+            }
+            else sum = nums[i];
+            
+        }
+        return ans;
+    }
+};
