beafa65ae7d7d178c59bed3688cfd9cd669b5fd2
Prajjwal Shukla <prajjwal.shukla44@gmail.com>
2026-08-13 23:11:52 +0530

    Add: ransom-note - submission-2

---
+++ Data Structures & Algorithms/ransom-note/submission-2.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int>frq;
+        for(char c: ransomNote) frq[c]++;
+        for(char c: magazine) if(frq.count(c))    frq[c]--;
+        for(auto it: frq)   if(it.second > 0)   return 0;
+        return 1;
+    }
+};
