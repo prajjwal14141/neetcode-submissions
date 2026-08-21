61c4f4e85f390cd3ea5322a7929e5b6f36a0f0c1
Prajjwal Shukla <prajjwal.shukla44@gmail.com>
2026-08-21 22:13:08 +0530

    Add: next-greater-element-i - submission-1

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums2.size();
+        vector<int> res;
+        for (int num : nums1) {
+            int nxt = -1;
+            for (int i = n - 1; i >= 0; i--) {
+                if (nums2[i] > num)   nxt = nums2[i];
+                else if (nums2[i] == num)   break;
+            }
+            res.push_back(nxt);
+        }
+        return res;
+    }
+};
